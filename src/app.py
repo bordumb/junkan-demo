3ade2208076e36f6377a803735464cc19f2a4338
bordumb <bordumbb@gmail.com>
2025-12-12 17:52:36 -0800

    Reset: Safe Baseline (V1)

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 import os
 # Expects payment_db_host from Terraform
 DB_HOST = os.getenv("PAYMENT_DB_HOST")
-
 def connect():
     if not DB_HOST:
         raise ValueError("Database host not configured!")
